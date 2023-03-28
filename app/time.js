cb2cc662044151a5e0b37fa9c4843fba2f13adeb
luc4sleite <lucahleite@gmail.com>
2023-03-27 22:47:52 -0300

    fix: correção do protocolo da api worldtime

--- app/time.js
+++ app/time.js
@@ -1,6 +1,6 @@
 async function showCurrentTime() {
   try {
-    const response = await fetch("http://worldtimeapi.org/api/ip");
+    const response = await fetch("https://worldtimeapi.org/api/ip");
     const data = await response.json();
 
     const datetime = data.datetime;
